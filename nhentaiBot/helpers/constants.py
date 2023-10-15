99e36bd3d5519e45d914966e1180fc65e0a710c3
KAC CHAN <kacchan@outlook.in>
2023-10-15 13:23:02 +0530

    Update constants.py

--- nhentaiBot/helpers/constants.py
+++ nhentaiBot/helpers/constants.py
@@ -2,7 +2,7 @@ import os
 
 
 # ------- GLOBAL VAR ----------
-BOT_TOKEN = os.environ.get('bot_token')
+BOT_TOKEN = os.environ.get('5811956762:AAGlP4ZHkTfE8X6fcKBvzwESobjWJ5thhoE')
 DEL_FAIL_LOG = "Unable to delete Message. Permissions NOT granted"
 S_SEARCH_DATA = {}
 SINGLE_MANGA_DATA = {}
